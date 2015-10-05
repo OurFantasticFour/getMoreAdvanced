7bca9c39f5cad7715a7fd3941e5e791ee2d8ee9a
Isak <isakglans@hotmail.com>
2015-10-05 13:08:50 +0200

    Isak ändrar 2

--- GetAdvancedFuxxers/GetAdvancedFuxxers/Program.cs
+++ GetAdvancedFuxxers/GetAdvancedFuxxers/Program.cs
@@ -11,7 +11,7 @@ namespace GetAdvancedFuxxers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world! I am apan Donald.");
+            Console.WriteLine("Hello world! I am apan Donald. Isak ÄNDRAR");
         }
     }
 }
